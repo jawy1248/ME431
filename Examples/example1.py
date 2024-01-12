044d278914459d9c6c4747c2e1e17f3a7a41b37c
jawy1248 <112504611+jawy1248@users.noreply.github.com>
2024-01-12 13:07:25 -0700

    First commit

---
+++ Examples/example1.py
@@ -0,0 +1,22 @@
+# %%(markdown)
+## Intro to importing functions in python
+
+# %%
+print('hello world')
+
+# %%
+a = [1, 2, 3]
+
+for i in a:
+    print(i)
+
+for i in range(len(a)):
+    print(a[i])
+
+# %%
+import numpy as np
+a = [1, 2, 3]
+b = [3, 2, 1]
+np.dot(a,b)
+
+# %%
