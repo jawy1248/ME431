b70a3725d8fbdb1a38491b44587e7acbfbcea4f6
jawy1248 <112504611+jawy1248@users.noreply.github.com>
2024-01-21 15:52:24 -0700

    Added HW2 animations

---
+++ controlsClass/_D_mass/python/HW2_Animation.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import massParam as P
+from signalGenerator import signalGenerator
+from massAnimation import massAnimation
+from dataPlotter import dataPlotter
+
+# instantiate reference input classes
+reference = signalGenerator(amplitude=0.5, frequency=0.2)
+zRef = signalGenerator(amplitude=3.0, frequency=0.2)
+fRef = signalGenerator(amplitude=5, frequency=.5)
+
+# instantiate the simulation plots and animation
+dataPlot = dataPlotter()
+animation = massAnimation()
+
+t = P.t_start  # time starts at t_start
+while t < P.t_end:  # main simulation loop
+    # set variables
+    r = 0
+    z = zRef.sin(t)
+    f = 0
+    # update animation
+    state = np.array([[z]])  #state is made of z
+    animation.update(state)
+    dataPlot.update(t, r, state, f)
+    # advance time by t_plot
+    t = t + P.t_plot  
+    plt.pause(0.001)  # allow time for animation to draw
+
+# Keeps the program from closing until the user presses a button.
+print('Press key to close')
+plt.waitforbuttonpress()
+plt.close()
---
+++ controlsClass/_F_planar_vtol/python/HW2_Animation.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import VTOLParam as P
+from signalGenerator import signalGenerator
+from VTOLAnimation import VTOLAnimation
+from dataPlotter import dataPlotter
+
+# instantiate reference input classes
+reference = signalGenerator(amplitude=0.5, frequency=0.2)
+zRef = signalGenerator(amplitude=1.0, frequency=0.1, y_offset=5)
+hRef = signalGenerator(amplitude=1.0, frequency=0.2, y_offset=5)
+thetaRef = signalGenerator(amplitude=np.pi, frequency=np.pi/10)
+fRef = signalGenerator(amplitude=5, frequency=.5)
+
+# instantiate the simulation plots and animation
+dataPlot = dataPlotter()
+animation = VTOLAnimation()
+
+t = P.t_start  # time starts at t_start
+while t < P.t_end:  # main simulation loop
+    # set variables
+    z = zRef.sin(t)
+    h = hRef.sin(t)
+    theta = thetaRef.sin(t)
+    f1 = 0
+    f2 = 0
+    # update animation
+    state = np.array([[z], [h], [theta]])  #state is made of z and theta
+    animation.update(state)
+    dataPlot.update(t, state, z, h, f1, f2)
+    # advance time by t_plot
+    t = t + P.t_plot  
+    plt.pause(0.001)  # allow time for animation to draw
+
+# Keeps the program from closing until the user presses a button.
+print('Press key to close')
+plt.waitforbuttonpress()
+plt.close()
